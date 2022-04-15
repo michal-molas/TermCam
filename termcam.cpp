1d54288774ab01dbb7f44b778f317df6f6a5ffb5
Michał Molas <molas.mich@gmail.com>
2022-04-15 22:24:30 +0200

    ssa

--- termcam.cpp
+++ termcam.cpp
@@ -1,6 +1,5 @@
 // Build with: g++ termcam.cpp -ljpeg -lz -o termcam
 
-#include <csignal>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
@@ -14,13 +13,17 @@
 
 #include "CImg.h"
 
-using namespace cimg_library;
-using namespace std;
-
 struct Pixel {
     unsigned int r;
     unsigned int g;
     unsigned int b;
+
+    Pixel &operator=(unsigned int n) {
+        r = 0;
+        g = 0;
+        b = 0;
+        return *this;
+    }
 
     Pixel &operator+=(const Pixel &other) {
         r += other.r;
@@ -38,95 +41,113 @@ struct Pixel {
 
     std::string print() const {
         std::string p = "\033[48;2;";
-        p += to_string(r);
+        p += std::to_string(r);
         p += ";";
-        p += to_string(g);
+        p += std::to_string(g);
         p += ";";
-        p += to_string(b);
+        p += std::to_string(b);
         p += "m ";
         return p;
     }
 };
 
-using frame_t = std::vector<std::vector<Pixel>>;
+class Frame {
+   private:
+    const size_t WIDTH = 640;
+    const size_t HEIGHT = 480 - 20;
 
-const size_t WIDTH = 640;
-const size_t HEIGHT = 480 - 20;
+    const size_t H_SCALE = 10;
+    const size_t W_SCALE = 5;
 
-const size_t H_SCALE = 10;
-const size_t W_SCALE = 5;
+    std::vector<std::vector<Pixel>> pixels;
 
-void print_frame(const frame_t &frame) {
-    std::string picture = "\033[48;2;0;0;0m";
-
-    for (size_t i = 0; i < frame.size(); i++) {
-        for (size_t j = frame[0].size(); j-- > 0;) {
-            picture += frame[i][j].print();
-        }
-        picture += "\033[48;2;0;0;0m\n";
-    }
-    picture += "\033[48;2;0;0;0m\n";
-    system("clear");
-    std::cout << picture;
-}
-
-void normalize(frame_t &frame) {
-    for (size_t i = 0; i < frame.size(); i++) {
-        for (size_t j = 0; j < frame[0].size(); j++) {
-            frame[i][j] /= H_SCALE * W_SCALE;
-        }
-    }
-}
-
-void shoot_frame(frame_t &frame) {
-    // Take a picture
-    system("mkdir temp 2>/dev/null");
-    system("fswebcam -r 640x480 --jpeg 85 -D 0 temp/web-cam-shot.jpg --quiet");
-
-    // Load image
-    CImg<unsigned char> img("temp/web-cam-shot.jpg");
-
-    // Delete the picture
-    system("rm temp/web-cam-shot.jpg");
-
-    // Dump all pixels
-    for (size_t y = 0; y < HEIGHT; y++) {
-        for (size_t x = 0; x < WIDTH; x++) {
-            frame[y / H_SCALE][x / W_SCALE] +=
-                Pixel({img(x, y, 0), img(x, y, 1), img(x, y, 2)});
+    void reset() {
+        for (size_t i = 0; i < pixels.size(); i++) {
+            for (size_t j = 0; j < pixels[0].size(); j++) {
+                pixels[i][j] = 0;
+            }
         }
     }
 
-    normalize(frame);
-}
-
-void write_frame(std::ofstream &os, const frame_t &frame) {
-    for (size_t i = 0; i < frame.size(); i++) {
-        for (size_t j = 0; j < frame[0].size(); j++) {
-            os << frame[i][j].r << " " << frame[i][j].g << " " << frame[i][j].b
-               << "\n";
-        }
-    }
-}
-
-bool read_frame(std::ifstream &is, frame_t &frame) {
-    for (size_t i = 0; i < frame.size(); i++) {
-        for (size_t j = 0; j < frame[0].size(); j++) {
-            if (!(is >> frame[i][j].r)) return false;
-            is >> frame[i][j].g >> frame[i][j].b;
+    void compress() {
+        for (size_t i = 0; i < pixels.size(); i++) {
+            for (size_t j = 0; j < pixels[0].size(); j++) {
+                pixels[i][j] /= H_SCALE * W_SCALE;
+            }
         }
     }
 
-    return true;
-}
+   public:
+    Frame() {
+        std::vector<Pixel> row(WIDTH / W_SCALE);
+        pixels = std::vector<std::vector<Pixel>>(HEIGHT / H_SCALE, row);
+    }
+
+    void print() {
+        std::string picture = "\033[48;2;0;0;0m";
+
+        for (size_t i = 0; i < pixels.size(); i++) {
+            for (size_t j = pixels[0].size(); j-- > 0;) {
+                picture += pixels[i][j].print();
+            }
+            picture += "\033[48;2;0;0;0m\n";
+        }
+        system("clear");
+        std::cout << picture;
+    }
+
+    void shoot() {
+        // Take a picture
+        system("mkdir temp 2>/dev/null");
+        system(
+            "fswebcam -r 640x480 --jpeg 85 -D 0 temp/web-cam-shot.jpg --quiet");
+
+        // Load image
+        cimg_library::CImg<unsigned char> img("temp/web-cam-shot.jpg");
+
+        // Delete the picture
+        system("rm temp/web-cam-shot.jpg");
+
+        reset();
+        // Dump all pixels
+        for (size_t y = 0; y < HEIGHT; y++) {
+            for (size_t x = 0; x < WIDTH; x++) {
+                pixels[y / H_SCALE][x / W_SCALE] +=
+                    Pixel({img(x, y, 0), img(x, y, 1), img(x, y, 2)});
+            }
+        }
+
+        compress();
+    }
+
+    void write(std::ofstream &os) {
+        for (size_t i = 0; i < pixels.size(); i++) {
+            for (size_t j = 0; j < pixels[0].size(); j++) {
+                os << pixels[i][j].r << " " << pixels[i][j].g << " "
+                   << pixels[i][j].b << "\n";
+            }
+        }
+    }
+
+    bool read(std::ifstream &is) {
+        reset();
+        for (size_t i = 0; i < pixels.size(); i++) {
+            for (size_t j = 0; j < pixels[0].size(); j++) {
+                if (!(is >> pixels[i][j].r)) return false;
+                is >> pixels[i][j].g >> pixels[i][j].b;
+            }
+        }
+
+        return true;
+    }
+};
 
 const std::string USAGE_MSG =
     "Usage:\n\t./termcam -s\n\t./termcam -r <number of frames> "
     "<filename>\n\t./termcam -p <filename>";
 
 int main(int argc, char **argv) {
-    std::vector<Pixel> row(WIDTH / W_SCALE);
-    frame_t frame(HEIGHT / H_SCALE, row);
+    Frame frame;
 
     if (argc == 4 && !strcmp(argv[1], "-r")) {  // record
         system("mkdir records 2>/dev/null");
@@ -135,26 +156,24 @@ int main(int argc, char **argv) {
         size_t no_frames = std::stoul(argv[2]);
 
         for (size_t i = 0; i < no_frames; i++) {
-            shoot_frame(frame);
-            write_frame(out_file, frame);
-            print_frame(frame);
+            frame.shoot();
+            frame.write(out_file);
+            frame.print();
         }
-        // TODO: handling ctrl + c
     } else if (argc == 3 && !strcmp(argv[1], "-p")) {  // play
         std::ifstream in_file(std::string("records/") + std::string(argv[2]));
         if (!in_file.is_open()) {
             std::cerr << "File doesn't exist" << std::endl;
             exit(EXIT_FAILURE);
         }
-        while (read_frame(in_file, frame)) {
+        while (frame.read(in_file)) {
             usleep(500000);
-            print_frame(frame);
+            frame.print();
         }
-        // TODO: safer reading (eg. by number of frames in first line)
     } else if (argc == 2 && !strcmp(argv[1], "-s")) {  // stream
         for (;;) {
-            shoot_frame(frame);
-            print_frame(frame);
+            frame.shoot();
+            frame.print();
         }
     } else {
         std::cerr << USAGE_MSG << std::endl;
